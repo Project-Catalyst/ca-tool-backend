c8e8cb663a7f272b807cbb57acff97486c78a9c6
Lucio Baglione <coire1@gmail.com>
2021-04-26 20:10:47 +0200

    Update scripts.

--- update-assessments-count.py
+++ update-assessments-count.py
@@ -1,27 +1,49 @@
 import random
 import json
+import requests
 from github import Github
 
 
 def loadOptions(goptions = {}):
     try:
-        with open('options.json', 'r') as f:
+        with open('./options.json', 'r') as f:
             options = json.load(f)
             for k in options:
-                setattr(goptions, k, options[k])
-    except:
+                goptions[k] = options[k]
+    except Exception as e:
+        print(e)
         print("Error loading options.json")
     return goptions
 
-def main():
-    goptions = loadOptions
-    g = Github(goptions['github_access_token'])
-    repo = g.get_repo(goptions['github_ca_backend_repo'])
-    contents = repo.get_contents("proposals.json", ref="test")
-
-    test_content = {
-        id: random.randrange(1, 100),
-        assessments: random.randrange(1, 5)
+def getAssessmentsCount(goptions):
+    headers = {
+        'api_token': goptions["ideascale_api_token"]
     }
+    ideas = []
+    for funnelStage in goptions["assess_funnel_stage_ids"]:
+        url = goptions["ideascale_base_api_url"] + \
+            goptions["assess_funnel_endpoint"].format(funnelStage)
 
-    repo.update_file(contents.path, "Update assessments count", json.dumps(test_content), contents.sha)
+        print("Requesting url: {}".format(url))
+        r = requests.get(url, headers=headers)
+        response = r.json()
+        for idea in response:
+            ideas.append({
+                "id": idea['ideaId'],
+                "assessments_count": idea['noOfAccessors']
+            })
+    return ideas
+
+def main():
+    goptions = loadOptions()
+    ideas = getAssessmentsCount(goptions)
+    if (len(ideas)):
+        try:
+            g = Github(goptions['github_access_token'])
+            repo = g.get_repo(goptions['github_ca_backend_repo'])
+            contents = repo.get_contents("proposals.json")
+            repo.update_file(contents.path, "Update assessments count", json.dumps(ideas), contents.sha)
+        except Exception as e:
+            print(e)
+
+main()
